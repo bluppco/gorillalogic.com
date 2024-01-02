f1ff7ed0dc0500f1f382eb5377b2d7d0eac8bbb2
Tanish Sharma <tanishsharmaab17@gmail.com>
2024-01-02 17:06:47 +0530

    feat: fix: updated styling of header component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,14 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			screens: {
+				'sm': '640px',
+				'md': '768px',
+				'lg': '1024px',
+				'xl': '1280px',
+				'mac': '1440px',
+				'2xl': '1536px'
+			},
 			fontFamily: {
 				'open_sans': ["Open Sans","sans-serif"],
 				'lora': ["Lora","serif"],
