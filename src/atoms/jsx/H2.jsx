5903d53e553f0b81ba299c0137f9a5c509866d91
Tanish Sharma <tanishsharmaab17@gmail.com>
2024-01-10 10:00:52 +0530

    enhancement: increased tracking width in paragraph and heading two components

--- src/atoms/jsx/H2.jsx
+++ src/atoms/jsx/H2.jsx
@@ -1,7 +1,7 @@
 const H2JSX = ( props ) => {
 
     return(
-        <h2 className="text-4xl md:text-5xl font-lora !leading-snug">
+        <h2 className="text-4xl md:text-5xl font-lora md:!leading-tight">
             { props.children }
         </h2>
 
