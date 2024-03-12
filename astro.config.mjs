737ddfe2fd1419f66c7b059c63ef6e4f179b5a5b
Tanish Sharma <tanishsharmaab17@gmail.com>
2024-03-12 11:38:17 +0530

    chore: upgraded astro version to latest

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,12 +7,20 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind(), react()],
-  output: "server",
-  adapter: cloudflare({
-    runtime: {
-      mode: 'local',
-      type: 'pages'
-    }
-  })
-});
+	integrations: [mdx(), tailwind(), react()],
+	output: "server",
+	adapter: cloudflare({
+		routes: {
+			exclude: [ "/icons/*", "/images/*", "/_astro/*", "/logo/*" ]
+		},
+		runtime: {
+			mode: 'local',
+			type: 'pages'
+		}
+	}),
+	image: {
+		service: {
+			entrypoint: "astro/assets/services/noop"
+		}
+	}
+})
